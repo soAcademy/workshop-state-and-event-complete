b2bbeab61e2b1178c9973cec4ea09c29bcc962ad
zigzagzone <zigzagzone@gmail.com>
2023-01-02 21:49:46 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import logo from './logo.svg'
 import './App.css'
-
-import EventComponent from './Event/EventComponent'
-import FirstCounterComponent from './Counter/FirstCounterComponent'
+import { UseStateComponent } from './UseState/UseStateComponent'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <EventComponent />
-        <FirstCounterComponent />
+        <UseStateComponent />
       </header>
     </div>
   )
